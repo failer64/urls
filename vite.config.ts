91750d10214ed56da591ce6834e9f0231a5720b1
failer64 <failer64@rambler.ru>
2024-02-07 00:15:17 +0300

    refactor config: add rollup

--- vite.config.ts
+++ vite.config.ts
@@ -5,9 +5,16 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react(), splitVendorChunkPlugin()],
 	base: '/urls',
-	// build: {
-	// 	rollupOptions: {
-	// 		// https://rollupjs.org/guide/en/#big-list-of-options
-	// 	}
-	// }
+	build: {
+		rollupOptions: {
+			// https://rollupjs.org/guide/en/#big-list-of-options
+			output: {
+				manualChunks(id) {
+					if (id.includes('node_modules')) {
+						return id.toString().split('node_modules/')[1].split('/')[0].toString();
+					}
+				}
+			}
+		}
+	}
 })
